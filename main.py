87c90e0f85dc396bca116093ae20770476c5a3b4
vancdk <74259267+vancdk@users.noreply.github.com>
2022-01-30 00:51:08 +0100

    Finalize record page
    Add action buttons

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
 from inaSpeechSegmenter import Segmenter
 from inaSpeechSegmenter.export_funcs import seg2csv, seg2textgrid
 
@@ -7,7 +7,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Félicitations, c'est en ligne!"
+	return render_template('index.html')
+
+@app.route("/record")
+def record_page():
+	return render_template('record.html')
+
 
 @app.route("/<media>")
 def voice_analysis(media):
@@ -15,6 +20,29 @@ def voice_analysis(media):
 	segmentation = seg(media)
 	segmentation_dispayed =  ', '.join(map(str,segmentation))
 	return segmentation_dispayed
+
+@app.route("/result", methods=["GET", "POST"])
+def result():
+	segmentation=""
+	if request.method == "POST":
+		print("AUDIO DATA RECEIVED")
+		if "file" not in request.files:
+			return redirect(request.url)
+
+		file = request.files["file"]
+		if file.filename == "":
+			return redirect(request.url)
+
+		if file:
+			seg = Segmenter()
+			segmentation = seg(file)
+			segmentation_dispayed =  ', '.join(map(str,segmentation))
+			print(segmentation_dispayed)
+	return render_template('result.html', segmentation=segmentation)
+
+
+
+
 
 
 if __name__ == "__main__":
