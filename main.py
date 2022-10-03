b94094c897d405a049b4dc4dfa9cebac0dd6fa25
vancdk <74259267+vancdk@users.noreply.github.com>
2022-10-03 22:18:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from inaSpeechSegmenter.export_funcs import seg2csv, seg2textgrid
 
 app = Flask(__name__)
 
+#HOTFIX On initialise une fois pour toutes au debut
+seg = Segmenter()
 
 @app.route("/")
 def index():
@@ -16,28 +18,39 @@ def record_page():
 
 @app.route("/<media>")
 def voice_analysis(media):
-	seg = Segmenter()
+	app.logger.info('voice analysis', media)
+#HOTFIX On  initialise une fois pour toutes au debut
+#	seg = Segmenter()
 	segmentation = seg(media)
 	segmentation_dispayed =  ', '.join(map(str,segmentation))
 	return segmentation_dispayed
 
 @app.route("/result", methods=["GET", "POST"])
 def result():
+	app.logger.info('IN RESULT')
 	segmentation=""
+	#HOTFIX  On enregistre le fichier en local tmp
 	if request.method == "POST":
 		print("AUDIO DATA RECEIVED")
-		if "file" not in request.files:
-			return redirect(request.url)
-
-		file = request.files["file"]
-		if file.filename == "":
-			return redirect(request.url)
-
-		if file:
-			seg = Segmenter()
-			segmentation = seg(file)
-			segmentation_dispayed =  ', '.join(map(str,segmentation))
-			print(segmentation_dispayed)
+		app.logger.info('IN RESULT POSTPOST')
+		print('request1', request)
+		print('request2', request.files)
+		print('request3', request.files['audio_data'])
+		request.files['audio_data'].save('/tmp/toto.wav')
+#		if "file" not in request.files:
+#			return redirect(request.url)
+#			
+#		print('request2', request.file)
+#		file = request.files["file"]
+#		if file.filename == "":
+#			return redirect(request.url)
+#
+#		if file:
+#		seg = Segmenter()
+		segmentation = seg('/tmp/toto.wav')
+		segmentation_dispayed =  ', '.join(map(str,segmentation))
+		print(segmentation_dispayed)
+	app.logger.info('IN RESULT END')
 	return render_template('result.html', segmentation=segmentation)
 
 
